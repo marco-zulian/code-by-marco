e4272ae4227b8b1051007ed523d69a9ba7d040d9
Marco Zulian <codebymarco1@gmail.com>
2025-07-10 18:13:40 -0300

    solves blog preview card challenge

--- blog-preview-card/blog-preview-card-main/components/blogPreviewCard.js
+++ blog-preview-card/blog-preview-card-main/components/blogPreviewCard.js
@@ -1,12 +1,24 @@
 class BlogPreviewCard extends HTMLElement {
   constructor() {
     super();
+    this._shadowRoot = this.attachShadow({ mode: 'open' });
 
-    const shadowRoot = this.attachShadow({ mode: 'open' });
+    this.render();
+  }
 
-    shadowRoot.innerHTML = `
+  render() {
+    this._shadowRoot.innerHTML = `
+      <link rel="stylesheet" href="./style/blog-preview-card-style.css">
       <div class="preview-card">
-        <img 
+        <img class="preview-card__image" />
+        <p class="preview-card__creation-date blog-preview-text-preset-3"></p>
+        <slot></slot>
+        <p class="preview-card__title blog-preview-text-preset-1"></p>
+        <p class="preview-card__subtitle blog-preview-text-preset-2"></p>
+        <div class="preview-card__author">
+          <img class="preview-card__author--image" />
+          <p class="preview-card__author--name blog-preview-text-preset-4"></p>
+        </div>
       </div>
     `;
   }
@@ -15,10 +27,54 @@ class BlogPreviewCard extends HTMLElement {
     this.updateContent();
   }
 
-  updateContent() {
+  static get observedAttributes() {
+    return ['img', 'src', 'title', 'subtitle', 'author', 'avatar-src', 'date'];
+  }
+
+  updateImage() {
+    const image = this.shadowRoot.querySelector('.preview-card__image');
     const imagePath = this.getAttribute('src') || null;
     if (imagePath !== null) {
       const imageAlt = this.getAttribute('alt') || null;
+      image.setAttribute('src', imagePath);
+      image.setAttribute('alt', imageAlt);
+      image.style.display = 'block';
+    } else {
+      image.style.display = 'none';
+    }
+  }
+
+  updateTexts() {
+    this.shadowRoot.querySelector('.preview-card__creation-date').textContent = `Published ${this.getAttribute('date') || ''}`;
+    this.shadowRoot.querySelector('.preview-card__title').textContent = this.getAttribute('title') || '';
+    this.shadowRoot.querySelector('.preview-card__subtitle').textContent = this.getAttribute('subtitle') || '';
+    this.shadowRoot.querySelector('.preview-card__author--name').textContent = this.getAttribute('author') || '';
+  }
+
+  updateAuthor() {
+    const avatarImage = this.shadowRoot.querySelector('.preview-card__author--image');
+    avatarImage.setAttribute('src', this.getAttribute('avatar-src') || '');
+  }
+
+  updateContent() {
+    this.updateImage();
+    this.updateTexts();
+    this.updateAuthor();
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    switch (name) {
+    case 'img', 'src':
+      this.updateImage();
+      break
+    case 'title', 'subtitle', 'author', 'date':
+      this.updateTexts();
+      break
+    case 'avatar-src':
+      this.updateAuthor();
+      break
     }
   }
 }
+
+customElements.define('cbm-blog-preview-card', BlogPreviewCard);
---
+++ blog-preview-card/blog-preview-card-main/components/blogPreviewTag.js
@@ -0,0 +1,20 @@
+class BlogPreviewTag extends HTMLElement {
+  constructor() {
+    super();
+    this._shadowRoot = this.attachShadow({ mode: 'open' });
+    this.render();
+  }
+
+  render() {
+    this._shadowRoot.innerHTML = `
+      <link rel="stylesheet" href="./style/blog-preview-tag-style.css">
+      <div class="preview-tag">
+        <p class="preview-tag__name blog-preview-text-preset-4">
+          <slot></slot>
+        </p>
+      </div>
+    `;
+  }
+}
+
+customElements.define('cbm-blog-preview-tag', BlogPreviewTag);
