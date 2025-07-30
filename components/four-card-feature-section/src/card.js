23cc8b0540b9f6d733f5c9199a5476bc3d89ebda
Marco Zulian <codebymarco1@gmail.com>
2025-07-30 19:46:50 -0300

    solves 4 cards challenge

---
+++ components/four-card-feature-section/src/card.js
@@ -0,0 +1,47 @@
+class Card extends HTMLElement {
+  constructor() {
+    super();
+    this._shadowRoot = this.attachShadow({ mode: 'open' });
+  }
+
+  render() {
+    this._shadowRoot.innerHTML = `
+      <link rel="stylesheet" href="../../styles/four-card-feature-section/card.css">
+      <div class="card">
+        <div class="card__text">
+          <p class="card__text--title text-preset-3">${this.getAttribute('title') || ''}</p>
+          <p class="card__text--description text-preset-5">${this.getAttribute('description') || ''}</p>
+        </div>
+        <img class="card__image"/>
+      </div>
+    `;
+
+    this.configureImage();
+  }
+
+  connectedCallback() {
+    this.render();
+  }
+  
+  static get observedAttributes() {
+    return ['title', 'description', 'src', 'alt'];
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    this.render();
+  }
+
+  configureImage() {
+    const image = this.shadowRoot.querySelector('.card__image');
+    const src = this.getAttribute('src') || '';
+
+    if (src === '') {
+      image.style.visibility = 'hidden';
+    } else {
+      image.setAttribute('src', src);
+      image.setAttribute('alt', this.getAttribute('alt') || '');
+    }
+  }
+}
+
+customElements.define('cbm-card', Card);
