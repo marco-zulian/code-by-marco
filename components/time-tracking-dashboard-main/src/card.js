a95df83ba2fc992cab302d41a4c5d17b53ae09b2
Marco Zulian <codebymarco1@gmail.com>
2025-08-27 16:55:16 -0300

    fixes font sizes, padding and gap on cards for lg bp

--- components/time-tracking-dashboard-main/src/card.js
+++ components/time-tracking-dashboard-main/src/card.js
@@ -22,13 +22,13 @@ let Card = class Card extends LitElement {
         return html `
       <div class="${this.getBackgroundColor()} relative overflow-hidden pt-9.5 md:pt-11.25 rounded-md">
         <img src="./assets/icon-${this.getIcon()}.svg" alt="" class="absolute right-4.5 top-0">        
-        <div class="flex flex-col gap-2 md:gap-4 bg-navy-900 hover:bg-navy-800 rounded-t-md p-8 md:p-6 z-10 relative">
+        <div class="flex flex-col gap-2 md:gap-4 lg:gap-6 bg-navy-900 hover:bg-navy-800 rounded-t-md p-8 md:p-6 lg:p-8 z-10 relative">
           <div class="flex justify-between items-center md:min-w-35">
             <p class="font-rubik font-medium text-white text-md/[21px]">${this.category}</p>
             <svg class="fill-navy-200 hover:fill-white cursor-pointer" width="21" height="5" xmlns="http://www.w3.org/2000/svg"><path d="M2.5 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Z" fill-rule="evenodd"/></svg>
           </div>
           <div class="flex justify-between items-center md:flex-col md:items-start md:gap-2">
-            <p class="font-rubik text-white font-light text-xl/[38px] md:text-2xl/[66px] md:align-top">${this.currAmount}hr${this.currAmount > 1 ? 's' : ''}</p>
+            <p class="font-rubik text-white font-light text-xl/[38px] md:text-3xl/[66px] md:align-top">${this.currAmount}hr${this.currAmount > 1 ? 's' : ''}</p>
             <p class="font-rubik text-navy-200 font-regular text-sm/[18px]">${this.getLastLabel()} - ${this.lastAmount}hr${this.lastAmount > 1 ? 's' : ''}</p>
           </div>
         </div>
