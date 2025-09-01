cc57c68e07e9418bbaba7747c35f5a34714693e6
Marco Zulian <codebymarco1@gmail.com>
2025-09-01 09:38:09 -0300

    font sizes and vertical positioning improvements

--- components/tip-calculator/src/resultPannel.ts
+++ components/tip-calculator/src/resultPannel.ts
@@ -27,13 +27,13 @@ export default class ResultPannel extends LitElement {
     return html`
       <div class="flex flex-col gap-6 justify-between rounded-md bg-green-900 px-8 py-6 lg:px-10 lg:py-9.5 h-full">
         <div class="flex flex-col gap-6">
-          <div class="flex justify-between">
+          <div class="flex justify-between items-center">
             <p class="font-bold font-space-mono text-md/[24px] text-white">Tip Amount<br><span class="text-sm/[19px] text-grey-400">/per person</span></p>
-            <p class="font-bold font-space-mono text-2xl text-green-400">${this._getPerPersonTip()}</p>
+            <p class="font-bold font-space-mono text-2xl/[47px] text-green-400 lg:text-3xl/[71px]">${this._getPerPersonTip()}</p>
           </div>
-          <div class="flex justify-between">
+          <div class="flex justify-between items-center">
             <p class="font-bold font-space-mono text-md/[24px] text-white">Total<br><span class="text-sm/[19px] text-grey-400">/per person</span></p>
-            <p class="font-bold font-space-mono text-2xl text-green-400">${this._getPerPersonPrice()}</p>
+            <p class="font-bold font-space-mono text-2xl/[47px] text-green-400 lg:text-3xl/[71px]">${this._getPerPersonPrice()}</p>
           </div>
         </div>
         
